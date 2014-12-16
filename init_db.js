7ed29f8baef9dabb0857579e2457f4e9a2624c20
injaon <gabriel.marcos.lopez@gmail.com>
2014-12-16 12:02:21 -0300

    agregue el obj ya reducido

--- init_db.js
+++ init_db.js
@@ -1,15 +1,16 @@
+"use strict";
 /*
  * Script for initialization the Database.
  * Usage: node init_db.js <new_records>
  */
 /* Funciones map y result de ejemplo */
 
-MAP = "investigador_map = function (k, v) {\r\n  self.log(\"inv in\");\r\n  var ms = 1000;\r\n  var started = new Date().getTime();\r\n  while((new Date().getTime() - started) < ms) {\r\n  }\r\n  self.emit(\"llave\", v*v);\r\n  self.log(\"inv in out\");\r\n};";
-REDUCE = "investigador_reduce = function (k, vals) {\r\n  var total = vals.reduce(function(a, b) {\r\n    return parseInt(a) + parseInt(b);\r\n  });\r\n  self.emit(k, total);\r\n};";
+var MAP = "investigador_map = function (k, v) {\r\n  self.log(\"inv in\");\r\n  var ms = 1000;\r\n  var started = new Date().getTime();\r\n  while((new Date().getTime() - started) < ms) {\r\n  }\r\n  self.emit(\"llave\", v*v);\r\n  self.log(\"inv in out\");\r\n};";
+var REDUCE = "investigador_reduce = function (k, vals) {\r\n  var total = vals.reduce(function(a, b) {\r\n    return parseInt(a) + parseInt(b);\r\n  });\r\n  self.emit(k, total);\r\n};";
 // clien: ireduce = function (k, vals) {var total = vals.reduce(function(a, b) {return parseInt(a) + parseInt(b);});  return total;};
 
 // sample object
-initial_object = {
+var initial_object = {
     // Datos originales provistos por el investigador.
     data: {
         0: 1,
@@ -50,34 +51,77 @@ initial_object = {
     ]
 };
 
-mapped_object = {
+var mapped_object = {
     available_slices: [0,  1],
-    data: { "0" : 1, "1" : 1, "2" : 2, "3" : 3 },
+    data: {"0": 1, "1": 1, "2": 2, "3": 3 },
     imap: MAP,
     ireduce: REDUCE,
-    map_results: { "0" : [     {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] },  {   "llave" : [     1,  1,  4 ] } ], "1" : [    {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] }, {    "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] }, {    "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] }, {    "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] },  {   "llave" : [     9 ] } ] },
+    map_results: {
+        "0" : [
+            {llave: [1, 1, 4]}, {llave: [1, 1, 4]}, {llave: [1, 1, 4]}, {llave: [1, 1, 4]},
+            {llave: [1, 1, 4]}
+        ], 
+        "1" : [
+            {llave: [9]}, {llave: [9]}, {llave: [9]}, {llave: [9]}, {llave: [9]}
+        ]
+    },
     reduce_data: {},
     reduce_results: {},
     result: {},
-    slices: [  {  "0" : 1,  "1" : 1,  "2" : 2 },  {  "3" : 3 } ] 
-}
+    slices: [{"0": 1, "1": 1, "2": 2 }, {"3": 3 }]
+};
+
+/* Objecto ya reducido. Listo para ser procesado y finalizado */
+var reduced_object = {
+    available_slices: [],
+    data: {"0": 1, "1": 1, "2": 2, "3": 3 },
+    imap: MAP,
+    ireduce: REDUCE,
+    map_results: {},
+    reduce_data: {
+        "llave" : [
+            1,
+            1,
+            4,
+            9
+        ]
+    },
+    reduce_results: {
+        "llave" : [
+            [15], [15], [15], [15], [15], [15]
+        ]
+    },
+    result: {},
+    slices: [{"0": 1, "1": 1, "2": 2 }, {"3": 3 }]
+};
 
 // imports
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert'),
     _ = require("underscore"),
     RECORDS = process.argv[2] || 10,
-    obj = process.argv[3] === "mapped"? mapped_object: initial_object;
-    url = 'mongodb://localhost:27017/tesis';
+    url = 'mongodb://localhost:27017/tesis',
+    obj = null;
+
+switch(process.argv[3]) {
+    case "mapped":
+        obj = mapped_object;
+        break;
+    case "reduced":
+        obj = reduced_object;
+        break;
+    default:
+    obj = initial_object;
+}
 
 // Connect and add records
 MongoClient.connect(url, function(err, conn) {
     assert.equal(null, err);
     console.log("Connected correctly to server");
-    workers = conn.collection("workers");
+    var workers = conn.collection("workers"),
+        arr = [];
 
-    var arr = [];
-    for(i = 0; i < RECORDS; i++)
+    for(var i = 0; i < RECORDS; i++)
         arr.push(_.clone(obj));
 
     workers.insert(arr, function (err, result) {
